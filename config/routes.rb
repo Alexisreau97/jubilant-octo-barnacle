f93781f1a6aa501f7d52f265b4e88671a9c34869
Alexisreau97 <alexis.reaupro@gmail.com>
2019-03-04 17:25:54 +0100

    add controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'items#index'
+  resources :items, only: %i[index show]
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
